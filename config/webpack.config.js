5b3209e7b067f38831a4fa976e3bd8d23ce00895
Omar <omar.bentaala@natecial.com>
2018-07-19 16:55:01 +0200

    first commit

---
+++ config/webpack.config.js
@@ -0,0 +1,32 @@
+// Import Webpack npm module
+var webpack = require('webpack');
+
+module.exports = {
+
+  mode: 'production',
+
+  // Which file types are in our project, and where they are located
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['src', 'node_modules'],
+  },
+  module: {
+    // How to process project files with loaders
+    rules: [
+      // Process any .js or .jsx file with Babel
+      { 
+        test: /\.jsx?$/, 
+        use: ['babel-loader'] 
+      }
+    ]
+  },
+  // Which file is the entry point to our application
+  entry: [
+    'index.jsx'
+  ],
+  // Where to output the final bundled code to
+  output: {
+    filename: 'app.js',
+    path: __dirname + "/../dist",
+  }
+};
